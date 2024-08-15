af5351be8c0e48a4504896901e3080445f74b3cb
itzah28 <adilaqbalhoda012@gmail.com>
2024-08-15 17:00:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,18 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+  API_ID = int(os.environ.get("29361361", ""))
+  API_HASH = os.environ.get("40314fdb92f99d22a1a84f4976f93913", "")
+  BOT_TOKEN = os.environ.get("7496617418:AAEOjfGhwWX1GtO_6WeYnuaVOt9dw6tIkQk", "")
+  BOT_USERNAME = os.environ.get("@itzahbot", "")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  SHORTLINK_URL = os.environ.get('udlinks.com', "")
+  SHORTLINK_API = os.environ.get('f5098f0d8b30285321147c63d99c545e047ba53c
+', "")
+  BOT_OWNER = int(os.environ.get("6187813986", ""))
+  DATABASE_URL = os.environ.get("mongodb+srv://adilaqbalhoda012:adil@123@cluster0.lzueu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
+  UPDATES_CHANNEL = os.environ.get("1002215534933", "")
+  LOG_CHANNEL = int(os.environ.get("1002215534933", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -32,7 +33,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [AH](https://www.instagram.com/itzah28/?hl=en)
  
  I am Super noob Please Support My Hard Work.
 
